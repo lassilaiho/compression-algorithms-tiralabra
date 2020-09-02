110a229cb4b53cb6fac97d1ebaa979a6c91e2f4a
Lassi Laiho <lassi.laiho0@gmail.com>
2020-09-02 22:26:29 +0300

    Update testutil

--- util/testutil/testutil.go
+++ util/testutil/testutil.go
@@ -20,6 +20,7 @@ func ReadFile(filename string) []byte {
 
 // ExpectNil fails the test if a != nil.
 func ExpectNil(t *testing.T, a interface{}) {
+	t.Helper()
 	if a != nil {
 		t.Fatalf("expected nil, got %v", a)
 	}
@@ -27,6 +28,7 @@ func ExpectNil(t *testing.T, a interface{}) {
 
 // ExpectEOF fails the test if err's error chain doesn't contain io.EOF.
 func ExpectEOF(t *testing.T, err error) {
+	t.Helper()
 	if !errors.Is(err, io.EOF) {
 		t.Fatalf("expected io.EOF, got %v", err)
 	}
@@ -34,6 +36,7 @@ func ExpectEOF(t *testing.T, err error) {
 
 // Check fails if expected and found are not equal.
 func Check(t *testing.T, expected, found interface{}) {
+	t.Helper()
 	if expected != found {
 		t.Fatalf("expected %v, found %v", expected, found)
 	}
